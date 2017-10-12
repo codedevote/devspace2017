c60f07ad5446e9362f45d6624a8033daa3b8e139
Florian Fordermaier <ff@ff-solutions.de>
2017-10-12 20:57:48 +0200

    cleanup

--- src/nodejs/src/server.js
+++ src/nodejs/src/server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const health = require('./api/health');
 const todo = require('./api/todo');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/_health', health(new express.Router()));
 app.use('/api', todo(new express.Router()));
 
 app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
